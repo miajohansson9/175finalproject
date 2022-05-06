0617639389fc0818941c9f27ae054eb98df1aaaa
Mia <miajohansson@college.harvard.edu>
2022-05-06 11:00:33 -0400

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float speed = 7f;
+    [SerializeField] float jumpForce = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector3(horizontalInput * speed, rb.velocity.y, rb.velocity.z);
+
+        if (Input.GetButtonDown("Jump")) {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+        
+    }
+}
