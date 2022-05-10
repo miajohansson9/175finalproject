128dbfdfcd453f0a29164bf813f80ccd4abc6229
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-05-10 15:26:35 -0400

    traps change x position
    traps now change x position and width

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     private float zPos;
+    private float xPos;
+    private float xDim;
 
     // Start is called before the first frame update
     void Start()
     {
+        xDim = Random.Range(0.1f, 0.19f);
+        transform.localScale = new Vector3(xDim, transform.localScale.y, transform.localScale.z);
         zPos = Random.Range(transform.parent.position.z - 8f, transform.parent.position.z + 8f);
-        transform.position = new Vector3(transform.position.x, transform.position.y, zPos);
+        xPos = Random.Range(transform.parent.position.x - ((transform.parent.lossyScale.x - transform.lossyScale.x)/2),
+                            transform.parent.position.x + ((transform.parent.lossyScale.x - transform.lossyScale.x)/2));
+        transform.position = new Vector3(xPos, transform.position.y, zPos);
     }
+
 }
