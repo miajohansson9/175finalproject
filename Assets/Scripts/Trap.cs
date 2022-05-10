0d9afada8a5fdbc966a2c312b9ebec7a052665d0
Victor Alvarez <victor.alvarez1218@gmail.com>
2022-05-10 19:31:57 -0400

    changed spike width

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -11,7 +11,7 @@ public class Trap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        xDim = Random.Range(0.1f, 0.19f);
+        xDim = Random.Range(0.05f, 0.19f);
         transform.localScale = new Vector3(xDim, transform.localScale.y, transform.localScale.z);
         zPos = Random.Range(transform.parent.position.z - 8f, transform.parent.position.z + 8f);
         xPos = Random.Range(transform.parent.position.x - ((transform.parent.lossyScale.x - transform.lossyScale.x)/2),
